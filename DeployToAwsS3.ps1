18f8001f1bfa6a57cfdcaddbce988309e5c74535
Anders Lundsgård <anders.lundsgard@scania.com>
2016-10-13 19:42:10 +0200

    Deploy script for AWS S3 hosting.

---
+++ DeployToAwsS3.ps1
@@ -0,0 +1,15 @@
+﻿## Pre-Req: 
+# aws configure
+# with access and security keys
+$awsprofile = "lundsgard"
+
+$bucketname = "www.continuousdelivery.eu"
+
+# Create bucket
+#aws s3 mb "s3://$bucketname"
+
+# Empty bucket first
+aws s3 rm "s3://$bucketname/" --recursive --profile $awsprofile
+
+# Create and Deploy package to s3
+aws s3 cp '.' "s3://$bucketname/" --recursive --exclude ".git/*" --exclude "*.ps1" --exclude ".idea/*" --exclude "*.md" --profile $awsprofile
