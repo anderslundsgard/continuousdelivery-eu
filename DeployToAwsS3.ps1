49efbba61d79d45158984bf59472df13ade8b9c4
Anders Lundsgard <anders.lundsgard@scania.com>
2016-12-26 21:28:41 +0100

    Make use of sync instead of delete and copy.

--- DeployToAwsS3.ps1
+++ DeployToAwsS3.ps1
@@ -8,8 +8,5 @@ $bucketname = "www.continuousdelivery.eu"
 # Create bucket
 #aws s3 mb "s3://$bucketname"
 
-# Empty bucket first
-aws s3 rm "s3://$bucketname/" --recursive --profile $awsprofile
-
-# Create and Deploy package to s3
-aws s3 cp '.' "s3://$bucketname/" --recursive --exclude ".git/*" --exclude "*.ps1" --exclude ".idea/*" --exclude "*.md" --profile $awsprofile
+# Sync package to s3
+aws s3 sync '.' "s3://$bucketname/" --exclude ".git/*" --exclude "*.ps1" --exclude ".idea/*" --exclude "*.md" --profile $awsprofile
